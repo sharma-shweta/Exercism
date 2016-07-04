f0a3d162c5e4db808161f46bdc664f30897714c8
Shweta Sharma <shweta.8448@gmail.com>
2016-07-04 23:24:02 +0800

    Diff of sq Ex

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,8 @@
+def difference(num):
+	return square_of_sum(num) - sum_of_squares(num)
+
+def square_of_sum(num):
+	return sum(n for n in xrange(num+1)) ** 2
+
+def sum_of_squares(num):
+	return sum(n**2 for n in xrange(num+1))
